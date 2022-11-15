f4ac07ef4aadd601b53081fa1ed5ce632190d145
Cédric Wagner <cwagner@tiz.fr>
2022-11-15 14:27:06 +0100

    component: create AutocompleteResult + write tests

---
+++ react_app/src/components/autocomplete-result/autocomplete-result.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './autocomplete-result.scss';
+
+const AutocompleteResult = ({items, onSelect}) => {
+  
+  function handleClick(e) {
+    onSelect(e.target.dataset.id);
+  }
+
+  return (
+    <ul className="autocomplete-result">
+      {items.map((item, key) => 
+        <li className="autocomplete-result__item" key={key} data-id={item.id} onClick={handleClick}>
+          {item.title}
+        </li>
+      )}
+    </ul>
+)};
+
+AutocompleteResult.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({ // @TODO: factorize this
+    title: PropTypes.string.isRequired,  
+    id: PropTypes.string.isRequired,
+  }))
+};
+
+AutocompleteResult.defaultProps = {};
+
+export default AutocompleteResult;
