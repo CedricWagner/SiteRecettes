0718f2af4fbbaa53650dc829d3cd96bfc0433188
Cédric Wagner <wagner.ced@gmail.com>
2024-01-21 18:52:22 +0100

    use search api for seasonal recipes

--- react_app/src/blocks/seasonal-recipes/season-recipes.api.js
+++ react_app/src/blocks/seasonal-recipes/season-recipes.api.js
@@ -1,14 +1,12 @@
+import { searchUrl } from "../../pages/search-result/search-result.api";
 import JsonApi from "../../utils/api/jsonapi";
-
-export const seasonalRecipesUrl = "/node/recipe";
 
 export default function getSeasonalRecipes(monthNumber, count) {
     const api = new JsonApi();
 
-    api.params.addFilter('status', '1');
-    api.params.addFilter('field_seasons.field_months.field_number', monthNumber, '=');
+    api.params.addFilter('field_season_months_number', monthNumber, '=');
     api.params.addPageLimit(count);
     api.params.addInclude(['field_image', 'field_categories']);
 
-    return api.get(seasonalRecipesUrl);
+    return api.get(searchUrl);
 }
